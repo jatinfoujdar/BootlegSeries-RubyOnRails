3f871f99c60ac91372a908cd4aa14cc01a7474ae
Jatin Foujdar <jatin321001@gmail.com>
2025-07-17 21:51:39 +0530

    method class

---
+++ Basics/Methods.rb
@@ -0,0 +1,14 @@
+def example_method
+  puts "This is an example method."
+end
+
+def area(length, width)
+  length * width
+end
+
+def area1
+  length = 5
+  width = 10
+  return length * width
+end
+
